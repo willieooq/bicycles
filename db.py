3f49b524224cb495d65377b9295be1311970d859
willieooq <willieooq@gmail.com>
2019-06-23 12:01:07 +0800

    A

--- db.py
+++ db.py
@@ -4,33 +4,24 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
 import psycopg2
+import uuid
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-class usermessage(db.Model):
-    __tablename__ = 'usermessage'
-    id = db.Column(db.String(50), primary_key=True)
-    user_id = db.Column(db.String(50))
-    message = db.Column(db.Text)
-    birth_date = db.Column(db.TIMESTAMP)
+class PictureDate(db.Model):
+    __tablename__ = 'PictureDate'
 
-    def __init__(self
-                 , id
-                 , user_id
-                 , message
-                 , birth_date
-                 ):
-        self.id = id
-        self.user_id = user_id
-        self.message = message
-        self.birth_date = birth_date
+    Id = db.Column(db.Integer, primary_key=True)
+    Uuid = db.Column(db.String(64), unique=True)
+    Title = db.Column(db.String(64))
+    Description = db.Column(db.String(128))
 
 
 if __name__ == '__main__':
