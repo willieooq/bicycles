02ea598f1e9f31face5ad98f2fe63ea678d84d17
willieooq <willieooq@gmail.com>
2019-06-24 14:55:11 +0800

    A

--- app.py
+++ app.py
@@ -189,6 +189,6 @@ con.close()
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='ec2-54-83-192-245.compute-1.amazonaws.com', port=port)
+    app.run(host='http://127.0.0.1', port=port)
 
 
