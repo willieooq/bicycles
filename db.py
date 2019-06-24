da916e2c2ef6d332e94f9b8c2804757eba5e9bbc
willieooq <willieooq@gmail.com>
2019-06-24 11:45:32 +0800

    a

---
+++ db.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+#conncect to the db
+
+con = psycopg2.connect(
+            host = 'ec2-54-83-192-245.compute-1.amazonaws.com',
+            database = 'df3vg11r7cab9s',
+            user = 'ouwlmxtvewdibl',
+            password ='05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131',
+            port = '5432')
+
+#cursor
+cur = con.cursor()
+
+cur.execute("insert into weather (city,date) values (%s, %s)",('Taiwan','2014-11-29' ))
+
+cur.execute("select city, date from weather")
+
+rows = cur.fetchall()
+
+#commit the transcation
+con.commit()
+
+#close the cur 
+cur.close()
+
+#close the connection
+con.close()
