28609d22192767246b77c181d02532586d6884af
willieooq <willieooq@gmail.com>
2019-06-24 15:10:47 +0800

    A

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def handle_message(event):
         line_bot_api.reply_message(Token,[TextSendMessage(text="您好，這是【廢棄腳踏車~重生!】活動大廳，小智機器人在此為您服務"),
                                           TemplateSendMessage(alt_text="Template Example1", template=title_btn)])
     elif (UserMsg == "開始舉報廢棄腳踏車"):
-        cur.execute("insert into bicycles (userid) values (UserId )")
+#        cur.execute("insert into bicycles (userid) values (UserId )")
         cur.execute("insert into bicycles (userid) values ('1' )")
         line_bot_api.reply_message(Token, TemplateSendMessage(alt_text="開始舉報廢棄腳踏車", template=str_btn))
         cur.execute("insert into bicycles (userid) values ('2' )")
