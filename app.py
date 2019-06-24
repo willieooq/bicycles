e90baf4cacfe9d5cfdc8e0bdd5f38f0b4ead55e9
willieooq <willieooq@gmail.com>
2019-06-24 14:41:52 +0800

    A

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def handle_message(event):
         line_bot_api.reply_message(Token,[TextSendMessage(text="您好，這是【廢棄腳踏車~重生!】活動大廳，小智機器人在此為您服務"),
                                           TemplateSendMessage(alt_text="Template Example1", template=title_btn)])
     elif (UserMsg == "開始舉報廢棄腳踏車"):
-        cur.execute("insert into bicycles (userid) values ('2' )")
+        #cur.execute("insert into bicycles (userid) values ('2' )")
         line_bot_api.reply_message(Token, [TemplateSendMessage(alt_text="開始舉報廢棄腳踏車", template=str_btn),
         TextSendMessage(text=UserId)])
         cur.execute("insert into bicycles (userid) values (UserId )")
@@ -189,6 +189,6 @@ con.close()
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='http://127.0.0.1', port=port)
+    app.run(host='ec2-54-83-192-245.compute-1.amazonaws.com', port=port)
 
 
