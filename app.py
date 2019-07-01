f3df23caef03d11517893d0469657660bec68df9
willieooq <willieooq@gmail.com>
2019-07-01 18:59:15 +0800

    A

--- app.py
+++ app.py
@@ -172,9 +172,7 @@ def handle_message(event):
     elif (UserMsg == "開始舉報廢棄腳踏車"):
         line_bot_api.reply_message(Token, [TextSendMessage(text="您尚未填寫聯絡資料，依照規定，請您提供聯絡人稱呼以及聯絡電話。您只需填寫一次，小智會記住，以後就可以直接舉報囉!\n\n舉報聯絡人:"+item['Name']+"\n聯絡電話:"+item['Num']),
                                           TemplateSendMessage(alt_text="開始舉報廢棄腳踏車", template=str_btn)])
-        if filter_UserId != None:
-            break
-        else:
+        if filter_UserId == None:
             insert_UserId = bicycles(UserId=item['User_Id'])
             db.session.add(insert_UserId)
             db.session.commit()
