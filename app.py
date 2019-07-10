59355785c07768f17b7d5010fa195ba40da29ce5
willieooq <willieooq@gmail.com>
2019-07-10 13:52:10 +0800

    A

--- app.py
+++ app.py
@@ -14,24 +14,20 @@ import psycopg2
 import datetime
 import os
 from dbmodle import *
+#db key
 app = Flask(__name__)
-#conncect to the db
-
-# con = psycopg2.connect(
-#             host = 'ec2-54-83-192-245.compute-1.amazonaws.com',
-#             database = 'df3vg11r7cab9s',
-#             user = 'ouwlmxtvewdibl',
-#             password ='05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131',
-#             port = '5432')
-# cur = con.cursor()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
+# imgur key
+client_id = '99d11a7b69e6d6d'
+client_secret = '93852968238cb8cf74aef86ecf66765b794d27a8'
+album_id = 'iXPWwiK'
+access_token = '6a615516a636718f7cf5d1b085fdc8d2f7ac415f'
+refresh_token = '47b3f8ae361407d25cf06cb281b7f5c93c332ff9'
 # Channel Access Token
 line_bot_api = LineBotApi("Z89KlbPxoc+N16dQw2gIOBUj1nht+r3FZLqjnHdGHX/WUZ8WpdvueISiYf+0J71JNll4ZJBw+D3QEHDjI8AwqxMMcS8dISHLl5YKn+FEyEnWp3Yt7pqE+Pl7hJ/5bgBSYOeyniI/pBKiD89LfE6+dwdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler('bd799d810b0b87531264f40763235c56')
