cd26d69c314a2ad015e6a1ff1d7e8553e618900c
willieooq <willieooq@gmail.com>
2019-07-10 14:01:35 +0800

    A

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-# from imgurpython import ImgurClient
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -14,6 +14,7 @@ import psycopg2
 import datetime
 import tempfile,os
 from dbmodle import *
+from imgur import *
 #db key
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s'
@@ -22,12 +23,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-# imgur key
-client_id = '99d11a7b69e6d6d'
-client_secret = '93852968238cb8cf74aef86ecf66765b794d27a8'
-album_id = 'iXPWwiK'
-access_token = '6a615516a636718f7cf5d1b085fdc8d2f7ac415f'
-refresh_token = '47b3f8ae361407d25cf06cb281b7f5c93c332ff9'
+
 # Channel Access Token
 line_bot_api = LineBotApi("Z89KlbPxoc+N16dQw2gIOBUj1nht+r3FZLqjnHdGHX/WUZ8WpdvueISiYf+0J71JNll4ZJBw+D3QEHDjI8AwqxMMcS8dISHLl5YKn+FEyEnWp3Yt7pqE+Pl7hJ/5bgBSYOeyniI/pBKiD89LfE6+dwdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler('bd799d810b0b87531264f40763235c56')
