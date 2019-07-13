a0cfe806613d7eb47a51c3f2ca9e7d5d429de594
willieooq <willieooq@gmail.com>
2019-07-13 20:43:32 +0800

    A

--- app.py
+++ app.py
@@ -201,6 +201,7 @@ def handle_message(event):
                 'description': 'Cute kitten being cute on '
             }
             path = os.path.join('static', 'tmp', dist_name)
+            client.upload_from_path(path, config=config, anon=False)
             image =client.upload_from_path(path, config=config, anon=False)
             os.remove(path)
             print(path)
