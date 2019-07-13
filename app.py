06c339a9d7e57fc36f5225c53978ae3120e75aa7
willieooq <willieooq@gmail.com>
2019-07-13 10:21:00 +0800

    A

--- app.py
+++ app.py
@@ -191,21 +191,21 @@ def handle_message(event):
         dist_path = tempfile_path + '.' + ext
         dist_name = os.path.basename(dist_path)
         os.rename(tempfile_path, dist_path)
-        try:
-            client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-            config = {
-                'album': album_id,
-                'name': 'Bike',
-                'title': 'Bike',
-                'description': 'Broken bicycles '
-            }
-            path = os.path.join('static', 'tmp', dist_name)
-            client.upload_from_path(path, config=config, anon=False)
-            os.remove(path)
-            print(path)
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='上傳成功'))
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='上傳失敗'))
+        # try:
+        client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+        config = {
+            'album': album_id,
+            'name': 'Bike',
+            'title': 'Bike',
+            'description': 'Broken bicycles '
+        }
+        path = os.path.join('static', 'tmp', dist_name)
+        client.upload_from_path(path, config=config, anon=False)
+        os.remove(path)
+        print(path)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='上傳成功'))
+        # except:
+        #     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='上傳失敗'))
         return 0
     # elif isinstance(event.message, VideoMessage):
     #     ext = 'mp4'
