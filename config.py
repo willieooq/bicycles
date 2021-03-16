2b755c15a1e11774ec98a15826ac719b093b28fd
willieooq <willieooq@gmail.com>
2021-03-16 19:10:42 +0800

    sercret repair

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ refresh_token = '47b3f8ae361407d25cf06cb281b7f5c93c332ff9'
 # line bot key
 line_channel_access_token ="Z89KlbPxoc+N16dQw2gIOBUj1nht+r3FZLqjnHdGHX/WUZ8WpdvueISiYf+0J71JNll4ZJBw+D3QEHDjI8AwqxMMcS8dISHLl5YKn+FEyEnWp3Yt7pqE+Pl7hJ/5bgBSYOeyniI/pBKiD89LfE6+dwdB04t89/1O/w1cDnyilFU="
 line_channel_secret ='bd799d810b0b87531264f40763235c56'
+
+# db
+SQLALCHEMY_DATABASE_URI = "postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s"
--- db.py
+++ db.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI;
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -21,7 +22,6 @@ class UserData(db.Model):
     total = db.Column(db.Integer)
     level = db.Column(db.Integer)
     user_msg = db.Column(db.String(64))
-    # bicycles = db.relationship("Bicycles", back_populates="userdata")
 
 class Bicycles(db.Model):
     __tablename__ = 'bicycles'
@@ -35,11 +35,6 @@ class Bicycles(db.Model):
     handler = db.Column(db.String(64))
     status = db.Column(db.String(64))
     updatedate = db.Column(db.TIMESTAMP)
-    # userdata = db.relationship("UserData", back_populates="bicycles")
-
-# test = Bicycles.query.filter(Bicycles.user_id == '666').first()
-# print('city',test.city)
-# print('name',test.userdata.name)
 
 if __name__ == '__main__':
     manager.run()
