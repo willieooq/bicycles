7d42acfe015ae04f77ab8437bcb4d4beb98c8f35
willieooq <willieooq@gmail.com>
2024-01-24 23:09:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # imgur key
-client_id = '99d11a7b69e6d6d'
-client_secret = '93852968238cb8cf74aef86ecf66765b794d27a8'
-album_id = 'iXPWwiK'
-access_token = '6a615516a636718f7cf5d1b085fdc8d2f7ac415f'
-refresh_token = '47b3f8ae361407d25cf06cb281b7f5c93c332ff9'
+client_id = 'client_id'
+client_secret = 'client_secret'
+album_id = 'album_id'
+access_token = 'access_token'
+refresh_token = 'refresh_token'
 
 # line bot key
-line_channel_access_token ="Z89KlbPxoc+N16dQw2gIOBUj1nht+r3FZLqjnHdGHX/WUZ8WpdvueISiYf+0J71JNll4ZJBw+D3QEHDjI8AwqxMMcS8dISHLl5YKn+FEyEnWp3Yt7pqE+Pl7hJ/5bgBSYOeyniI/pBKiD89LfE6+dwdB04t89/1O/w1cDnyilFU="
-line_channel_secret ='bd799d810b0b87531264f40763235c56'
+line_channel_access_token ="line_channel_access_token"
+line_channel_secret ='line_channel_secret'
 
 # db
-SQLALCHEMY_DATABASE_URI = "postgres://ouwlmxtvewdibl:05f09b74d57c0cf93c2594966a1e03e06c7ba3605d56b46d8ecce6f61da50131@ec2-54-83-192-245.compute-1.amazonaws.com:5432/df3vg11r7cab9s"
+SQLALCHEMY_DATABASE_URI = "SQLALCHEMY_DATABASE_URI"
